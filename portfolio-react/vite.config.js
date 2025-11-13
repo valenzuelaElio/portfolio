77d34f891258a1c156f6ef85927142e3b64aa72d
Elio Valenzuela Muñoz <valenzuela.eliogd@gmail.com>
2025-11-13 20:27:35 -0300

    update

--- portfolio-react/vite.config.js
+++ portfolio-react/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://ElioValenzuela.github.io/portfolio"
+  base:"https://valenzuelaElio.github.io/portfolio"
 })
